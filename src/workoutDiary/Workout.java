30f1d1caac3cfa26dbf8574f2b0da47a59e3a272
monikahm <monika.muzyk@hotmail.com>
2019-03-16 15:39:49 +0100

    Connection manager

--- src/workoutDiary/Workout.java
+++ src/workoutDiary/Workout.java
@@ -1,4 +1,68 @@
 package workoutDiary;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 
 public class Workout {
+    private final static String sqlUrl = "jdbc:mysql://localhost/monikahm_prosjekt";
+
+    private final static String userName = "root";
+    private final static String password = "";
+
+
+
+    public static void main(String[] argv) {
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Where is your MySQL JDBC Driver?");
+            e.printStackTrace();
+            return;
+        }
+
+        System.out.println("MySQL JDBC Driver Registered!");
+        Connection connection = null;
+
+        try {
+            connection = DriverManager
+                    .getConnection(sqlUrl, root, password);
+
+        } catch (SQLException e) {
+            System.out.println("Connection Failed! Check output console");
+            e.printStackTrace();
+            return;
+        }
+
+        if (connection != null) {
+            System.out.println("You made it, take control your database now!");
+        } else {
+            System.out.println("Failed to make connection!");
+        }
+
+        Statement stmt;
+        try {
+            stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM apparat");
+            System.out.println(rs.getString(0));
+
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+
 }
